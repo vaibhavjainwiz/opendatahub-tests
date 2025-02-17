5bdfc24a02c525ac27b91ac822b2f1fa898f09d6
Ruth Netser <rnetser@redhat.com>
2025-02-17 16:04:25 +0200

    utilities/manifests/vllm.py - update response regex (#142)
    * Create size-labeler.yml
    
    * Delete .github/workflows/size-labeler.yml
    
    * model mesh - add auth tests
    
    * xx
    
    * update vllm reposnse regex
    
    ---------
    
    Co-authored-by: Luca Giorgi <lgiorgi@redhat.com>

--- utilities/manifests/vllm.py
+++ utilities/manifests/vllm.py
@@ -1,7 +1,7 @@
 VLLM_INFERENCE_CONFIG = {
         "default_query_model": {
             "query_input": '"prompt": "At what temperature does Nitrogen boil?", "max_tokens": 100, "temperature": 0',
-            "query_output": r'{"id":"cmpl-[a-z0-9]+","object":"text_completion","created":\d+,"model":"$model_name","choices":\[{"index":0,"text":".*Theboilingpointofnitrogenis77.4K.*","logprobs":null,"finish_reason":"length","stop_reason":null,"prompt_logprobs":null}\],"usage":{"prompt_tokens":10,"total_tokens":110,"completion_tokens":100}}',
+            "query_output": r'{"id":"cmpl-[a-z0-9]+","object":"text_completion","created":\d+,"model":"$model_name","choices":\[{"index":0,"text":".*Theboilingpointofnitrogenis77.4K.*","logprobs":null,"finish_reason":"length","stop_reason":null,"prompt_logprobs":null}\],"usage":{"prompt_tokens":10,"total_tokens":110,"completion_tokens":100,"prompt_tokens_details":null}}',
             "use_regex": True
         },
         "completions": {
