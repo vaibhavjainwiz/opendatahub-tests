497cbb0ee1f6b3ceba392757ad68c4c8f0c55a74
Federico Mosca <fmosca@redhat.com>
2025-04-11 10:02:58 +0200

    change: Update required python version to 3.12.x
    * change: Update required python version
    
    * change: update version to 3.12
    
    * change: update uv.lock
    
    * change: remove future imports
    
    * fix: fix the version to 3.12.x
    
    ---------
    
    Co-authored-by: Debarati Basu-Nag <dbasunag@redhat.com>

--- tests/workbenches/utils.py
+++ tests/workbenches/utils.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from kubernetes.dynamic import DynamicClient, Resource, ResourceInstance
 
 
--- utilities/certificates_utils.py
+++ utilities/certificates_utils.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import base64
 import os
 from functools import cache
