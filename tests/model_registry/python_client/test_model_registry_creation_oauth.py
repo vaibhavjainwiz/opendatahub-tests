9be1e4bbf0eb2c39950bf3c433f7cf43a96ac7ee
Luca Giorgi <lgiorgi@redhat.com>
2025-06-05 15:02:30 +0200

    test MR failure when using http protocol for endpoint (#336)
    * feat: test failure when using http protocol
    
    Signed-off-by: lugi0 <lgiorgi@redhat.com>
    
    * fix: typo and small refactor
    
    Signed-off-by: lugi0 <lgiorgi@redhat.com>
    
    ---------
    
    Signed-off-by: lugi0 <lgiorgi@redhat.com>

--- tests/model_registry/python_client/test_model_registry_creation_oauth.py
+++ tests/model_registry/python_client/test_model_registry_creation_oauth.py
@@ -7,13 +7,18 @@ from utilities.infra import get_product_version
 from utilities.constants import DscComponents
 from tests.model_registry.constants import MODEL_NAME, MODEL_DICT
 from model_registry import ModelRegistry as ModelRegistryClient
+from kubernetes.dynamic import DynamicClient
+from ocp_resources.service import Service
+from utilities.constants import Protocols
+from tests.model_registry.utils import get_endpoint_from_mr_service
+from aiohttp.client_exceptions import ServerDisconnectedError
 
 LOGGER = get_logger(name=__name__)
 MINVER = Version.parse(version="2.21.0")
 
 
 @pytest.mark.parametrize(
-    "updated_dsc_component_state_scope_class, model_registry_client",
+    "updated_dsc_component_state_scope_class",
     [
         pytest.param(
             {
@@ -24,7 +29,6 @@ MINVER = Version.parse(version="2.21.0")
                     },
                 },
             },
-            {"service_fixture": "model_registry_instance_oauth_service"},
             id="oauth_proxy",
         ),
     ],
@@ -33,18 +37,28 @@ MINVER = Version.parse(version="2.21.0")
 @pytest.mark.usefixtures("updated_dsc_component_state_scope_class")
 class TestModelRegistryCreationOAuth:
     """
-    Tests the creation of a model registry with OAuth proxy configuration.
-    Jira ID: RHOAIENG-26194
+    Validate model registry with OAuth proxy configuration.
+    Jira IDs: RHOAIENG-26194, RHOAIENG-26195
     """
 
+    # Tests RHOAIENG-26194
+    @pytest.mark.parametrize(
+        "model_registry_client",
+        [
+            pytest.param(
+                {"service_fixture": "model_registry_instance_oauth_service"},
+            )
+        ],
+        indirect=True,
+    )
     @pytest.mark.smoke
     def test_registering_model_with_oauth(
         self: Self,
-        admin_client,
+        admin_client: DynamicClient,
         model_registry_client: ModelRegistryClient,
     ):
         if py_config["distribution"] == "downstream" and get_product_version(admin_client=admin_client) < MINVER:
-            pytest.skip("Skipping test for RHOAI < 2.21")
+            pytest.skip(f"Skipping test for RHOAI < {MINVER}")
 
         # Register a new model
         registered_model = model_registry_client.register_model(
@@ -75,3 +89,30 @@ class TestModelRegistryCreationOAuth:
         ]
         if errors:
             pytest.fail("errors found in model registry response validation:\n{}".format("\n".join(errors)))
+
+    # Tests RHOAIENG-26195
+    def test_encrypted_oauth_proxy(
+        self: Self,
+        admin_client: DynamicClient,
+        model_registry_instance_oauth_service: Service,
+        current_client_token: str,
+    ):
+        """Test that connecting to encrypted OAuth proxy with HTTP protocol fails appropriately."""
+        if py_config["distribution"] == "downstream" and get_product_version(admin_client=admin_client) < MINVER:
+            pytest.skip(f"Skipping test for RHOAI < {MINVER}")
+
+        # Get the REST endpoint
+        rest_endpoint = get_endpoint_from_mr_service(svc=model_registry_instance_oauth_service, protocol=Protocols.REST)
+
+        # Create the client
+        server, port = rest_endpoint.split(":")
+        with pytest.raises(ServerDisconnectedError) as exc_info:
+            _ = ModelRegistryClient(
+                server_address=f"{Protocols.HTTP}://{server}",
+                port=port,
+                author="opendatahub-test",
+                user_token=current_client_token,
+                is_secure=False,
+            )
+        assert str(exc_info.value) == "Server disconnected", f"Expected Server disconnected, but got {exc_info.value}"
+        LOGGER.info("Successfully received expected Server Disconnected exception")
